6932ee454ee3fe8ef485d6ef7895054668bba8ce
jorgegioya <gioyajorge@gmail.com>
2026-05-07 21:58:18 +0200

    Modificada cabecera en main

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         while (!salir) {
 
-            System.out.println("\n=== GESTOR DE NOTAS ===");
+            System.out.println("=== GESTOR DE NOTAS - VERSION PRINCIPAL ===");
             System.out.println("1. Crear nota");
             System.out.println("2. Listar notas");
             System.out.println("3. Listar importantes");
