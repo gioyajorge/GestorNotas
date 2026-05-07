f0deb011a566f1ee082cb20fb8128bc043dad8cf
jorgegioya <gioyajorge@gmail.com>
2026-05-07 20:46:09 +0200

    En esta rama, desarrollar la estructura básica de la aplicación: o clase Nota con atributos o constructor o getters y setters o clase GestorNotas con colección interna de notas o clase Main con arranque básico del programa

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        GestorNotas gestor = new GestorNotas();
+
+        boolean salir = false;
+
+        while (!salir) {
+
+            System.out.println("\n=== GESTOR DE NOTAS ===");
+            System.out.println("1. Crear nota");
+            System.out.println("2. Listar notas");
+            System.out.println("3. Listar importantes");
+            System.out.println("0. Salir");
+
+            String opcion = scanner.nextLine();
+
+            switch (opcion) {
+                case "1":
+                    // TODO: implementar
+                    break;
+                case "2":
+                    // TODO: implementar
+                    break;
+                case "3":
+                    // TODO: implementar
+                    break;
+                case "0":
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+            }
+        }
+
+        scanner.close();
+    }
 }
